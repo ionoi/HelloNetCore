abdef8c351035c7befc8193de8a8dcf49c144b02
ionoi <bonfy222@163.com>
2020-06-19 15:06:04 +0800

    update

--- Devops/jenkins.groovy
+++ Devops/jenkins.groovy
@@ -1,25 +1,89 @@
 pipeline {
     agent any
 
-    options
-    {
-        timestamps()
+    environment {
+        def SeoPath = "${env.seo_root}"
+        def DataFolderName = "data"
+        def DataPath = "${SeoPath}/${DataFolderName}"
+
+        def OutPutFolderName = "output"
+        def OutputPath = "${SeoPath}/${OutPutFolderName}"
+
+
     }
 
+
+    options
+	{
+		timestamps()
+        skipDefaultCheckout()
+        buildDiscarder(logRotator(numToKeepStr: "30", daysToKeepStr: '7'))
+	}
+
+        parameters {
+            choice (name : 'cleanup', choices : 'no\nyes', description : 'cleanup the data folder?')
+        }
+
     stages {
-        stage('Run') {
+        stage('Prepare') {
             steps {
                 script {
-                    echo "Hello Jenkins"
+                    echo "INFO: Check and Create Data Folder"
+                    CheckAndCreate(${SeoPath}, ${DataFolderName})
+                    CheckAndCreate(${SeoPath}, ${OutPutFolderName})
+                }
+            }
+        }
 
+        stage('aws') {
+            steps {
+                script {
+                    echo "Zip"
                     bat '''
                     dir
-                    cd HelloNetCore
-                    dotnet run
+                    aws --version
                     '''
 
                 }
             }
         }
+
+        stage('Run') {
+            steps {
+                script {
+                    echo "Hello Jenkins"
+
+                    // bat '''
+                    // dir
+                    // cd HelloNetCore
+                    // dotnet run
+                    // '''
+
+                }
+            }
+        }
+
     }
+
+   
+    post {
+        success {            
+            echo 'INFO: Success'
+        }
+        always {
+            deleteDir()
+        }
+    }
+}
+
+
+def CheckAndCreate(path, name) {
+    def folder_name = "${path}/${name}" 
+    bat '''
+    if (-not (Test-Path ${filename})) {
+        New-Item  -Path ${path} -Name "${folder_name}" -ItemType "directory"
+        echo "create folder: ${folder_name}"
+    } else {
+        echo "${folder_name} already exists"
+    }'''
 }
